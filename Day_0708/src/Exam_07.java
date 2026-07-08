2ba84a72605eaf43bd8b33af4315dc64582d4d0d
레노버7 <7@DESKTOP-RL8V21L>
2026-07-08 17:46:57 +0900

    7월 8일 최종

---
+++ Day_0708/src/Exam_07.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exam_07 {
+
+	public static void main(String[] args) {
+		Scanner scnner = new Scanner(System.in);
+		System.out.print("1. 입력하시오");
+		//String str1 = scanner.next();
+		//String str2 = scanner.next();
+		
+		//System.out.print("1. 입력하시오");
+		String str3 = scanner.nextLine();
+		
+		//System.out.println("str1 : " + str1);
+		//System.out.println("str2 : " + str2);
+		System.out.println("str3 : " + str3);
+		
+
+	}
+
+}
