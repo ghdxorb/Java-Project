05a9a2dc25e019c8a45e0adacc6407d364b221e8
레노버7 <7@DESKTOP-RL8V21L>
2026-07-10 12:51:51 +0900

    7월 10일 오전

---
+++ Day_0710/src/Exam_02.java
@@ -0,0 +1,29 @@
+//1.키보드로 점수 입력 입력된 점수가 80~90 사이면 "Pass"
+//2.키보드로 점수 입력 입력된 점수가 80~90 사이면 "Pass"
+//	아니면 "NoPass"
+
+import java.util.Scanner;
+
+public class Exam_02 {
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		
+		System.out.print("입력:");
+		int score = scn.nextInt();
+		
+		//&& 사용시
+		if(score >=80&&score <=90) {
+			System.out.println("&&(and) 사용시 Pass");
+		}
+		
+		//&& 미사용시
+		if(score>=80) {
+			if(score<=90) {
+				System.out.println("&&(and) 사용시 Pass");	
+			}
+		}
+	}
+
+
+	}
+
---
+++ Day_0710/src/Exam_08_2.java
@@ -0,0 +1,6 @@
+
+public class Exam_08_2 {
+	public static void main(String[] args) {
+		
+	}
+}
