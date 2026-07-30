b5418ef91b82ced2c57b7a06a8d48f4cda7de842
레노버7 <7@DESKTOP-RL8V21L>
2026-07-30 12:49:07 +0900

    7월 30일 오전

---
+++ Day_0730/src/com/mnu/exam01/Student.java
@@ -0,0 +1,23 @@
+package com.mnu.exam01;
+
+public class Student {
+	int bun;
+	String name;
+	int kor;
+	int eng;
+	int mat;
+
+	
+	Student(int bun, String name,int kor,int eng,int mat){
+		this.bun=bun;
+		this.name=name;
+		this.kor=kor;
+		this.eng=eng;
+		this.mat=mat;
+
+	}
+
+	public getBun();{
+		
+	}
+}
